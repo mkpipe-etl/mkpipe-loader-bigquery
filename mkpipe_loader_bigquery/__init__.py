d942afc8e9c0dff215890cefe63f1519fa524b24
m-karakus <s.metinkarakus@gmail.com>
2026-05-06 23:00:49 +0300

    feature: if_existes parameters added

--- mkpipe_loader_bigquery/__init__.py
+++ mkpipe_loader_bigquery/__init__.py
@@ -148,7 +148,8 @@ class BigQueryLoader(BaseLoader, variant='bigquery'):
                 case WriteStrategy.APPEND:
                     self._write_df(df, 'append', target_name)
                 case WriteStrategy.REPLACE:
-                    self._write_df(df, 'overwrite', target_name)
+                    mode = 'append' if self.if_exists == 'append' else 'overwrite'
+                    self._write_df(df, mode, target_name)
                 case WriteStrategy.UPSERT | WriteStrategy.MERGE:
                     if not table.write_key:
                         raise ConfigError(
